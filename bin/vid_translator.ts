9c07141afb7307e21f6c988c87a797afab350b7e
Gilbert Young Jr <gilbertjyoungjr@gmail.com>
2023-06-19 07:05:56 -0600

    Initial changes

--- bin/vid_translator.ts
+++ bin/vid_translator.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { VidTranslatorStack } from '../lib/vid_translator-stack';
+import "source-map-support/register";
+import * as cdk from "aws-cdk-lib";
+import { VidTranslatorStack } from "../lib/vid_translator-stack";
 
 const app = new cdk.App();
-new VidTranslatorStack(app, 'VidTranslatorStack', {
+new VidTranslatorStack(app, "VidTranslatorStack", {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
@@ -15,7 +15,7 @@ new VidTranslatorStack(app, 'VidTranslatorStack', {
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+  env: { account: "562569850718", region: "us-east-1" },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
--- lib/vid_translator-stack.ts
+++ lib/vid_translator-stack.ts
@@ -1,16 +1,12 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import { Bucket } from "aws-cdk-lib/aws-s3";
+import { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class VidTranslatorStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'VidTranslatorQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const bucket = new Bucket(this, "VideosBucket", {});
   }
 }
